0bb45b66a10ec3fab1e64f4343b04e4f97485014
utigernils <utigernils@gmail.com>
2026-04-10 11:32:18 +0200

    refactor; update web api to match new standarts

--- MirrorClock_Firmware/web_api.h
+++ MirrorClock_Firmware/web_api.h
@@ -16,7 +16,7 @@ private:
     LightSensor& sensor;
 
     String createJsonResponse(bool success, String message = "", String data = "") {
-        DynamicJsonDocument doc(1024);
+        DynamicJsonDocument doc(2048);
         doc["success"] = success;
         doc["timestamp"] = timeMgr.getEpochTime();
         
@@ -25,7 +25,7 @@ private:
         }
         
         if (data != "") {
-            DynamicJsonDocument dataDoc(512);
+            DynamicJsonDocument dataDoc(1024);
             deserializeJson(dataDoc, data);
             doc["data"] = dataDoc;
         }
@@ -35,215 +35,181 @@ private:
         return response;
     }
 
-    void handleStatus() {
-        DynamicJsonDocument doc(2048);
+    void handleGetStatus() {
+        DynamicJsonDocument doc(1024);
         
         doc["system"]["uptime"] = millis();
         doc["system"]["free_heap"] = ESP.getFreeHeap();
         doc["system"]["chip_id"] = ESP.getChipId();
-        doc["system"]["flash_size"] = ESP.getFlashChipSize();
         doc["system"]["cpu_freq"] = ESP.getCpuFreqMHz();
         
         doc["wifi"]["connected"] = network.isConnected();
         doc["wifi"]["ssid"] = network.getSSID();
         doc["wifi"]["ip"] = network.getIP();
         doc["wifi"]["rssi"] = network.getRSSI();
-        doc["wifi"]["mac"] = network.getMacAddress();
         
-        doc["time"]["hour"] = timeMgr.getHour();
-        doc["time"]["minute"] = timeMgr.getMinute();
         doc["time"]["epoch"] = timeMgr.getEpochTime();
-        doc["time"]["ntp_server"] = NTP_SERVER;
-        doc["time"]["timezone"] = TIMEZONE;
+        doc["time"]["formatted"] = String(timeMgr.getHour()) + ":" + String(timeMgr.getMinute());
         
-        doc["led"]["enabled"] = LED_ENABLED;
-        doc["led"]["brightness"] = LED_BRIGHTNESS;
-        doc["led"]["auto_brightness"] = LED_AUTO_BRIGHTNESS;
-        doc["led"]["color"]["r"] = LED_R;
-        doc["led"]["color"]["g"] = LED_G;
-        doc["led"]["color"]["b"] = LED_B;
-        doc["led"]["count"] = LED_COUNT;
-        doc["led"]["pin"] = LED_PIN;
-        
-        doc["light_sensor"]["lux"] = sensor.getLightLevel();
-        doc["light_sensor"]["auto_brightness"] = AUTO_BRIGHTNESS_ENABLED;
-        doc["light_sensor"]["calculated_brightness"] = sensor.getBrightness();
-        
-        doc["config"]["update_interval"] = UPDATE_INTERVAL;
-        doc["config"]["web_port"] = WEB_SERVER_PORT;
+        doc["sensor"]["lux"] = sensor.getLightLevel();
+        doc["sensor"]["calculated_brightness"] = sensor.getBrightness();
         
         String response;
         serializeJson(doc, response);
-        server.send(200, "application/json", response);
+        server.send(200, "application/json", createJsonResponse(true, "Status retrieved", response));
     }
 
-    void handlePower() {
-        #if DEBUG_ENABLED
-            Serial.println(DEBUG_PREFIX_WEB "API call: /api/power");
-        #endif
-        
-        if (!server.hasArg("plain")) {
-            server.send(400, "application/json", createJsonResponse(false, "No JSON body provided"));
-            return;
-        }
-        
-        String body = server.arg("plain");
-        DynamicJsonDocument doc(256);
-        DeserializationError error = deserializeJson(doc, body);
-        
-        if (error) {
-            server.send(400, "application/json", createJsonResponse(false, "Invalid JSON"));
-            return;
-        }
-        
-        if (!doc.containsKey("enabled")) {
-            server.send(400, "application/json", createJsonResponse(false, "Missing 'enabled' parameter"));
-            return;
-        }
-        
-        LED_ENABLED = doc["enabled"];
-        
-        #if DEBUG_ENABLED
-            Serial.println(DEBUG_PREFIX_LED "Power state changed: " + String(LED_ENABLED ? "ON" : "OFF"));
-        #endif
-        
-        if (!LED_ENABLED) {
-            leds.clear();
-            leds.show();
-        } else {
-            extern int lastHour, lastMinute;
-            lastHour = -1;
-            lastMinute = -1;
-        }
-        
-        DynamicJsonDocument responseData(128);
-        responseData["enabled"] = LED_ENABLED;
-        String dataStr;
-        serializeJson(responseData, dataStr);
-        
-        server.send(200, "application/json", createJsonResponse(true, "Power state updated", dataStr));
-    }
-
-    void handleSetBrightness() {
-        #if DEBUG_ENABLED
-            Serial.println(DEBUG_PREFIX_WEB "API call: /api/brightness");
-        #endif
-        
-        if (!server.hasArg("plain")) {
-            server.send(400, "application/json", createJsonResponse(false, "No JSON body provided"));
-            return;
-        }
-        
-        String body = server.arg("plain");
-        DynamicJsonDocument doc(256);
-        DeserializationError error = deserializeJson(doc, body);
-        
-        if (error) {
-            server.send(400, "application/json", createJsonResponse(false, "Invalid JSON"));
-            return;
-        }
-        
-        if (!doc.containsKey("brightness")) {
-            server.send(400, "application/json", createJsonResponse(false, "Missing 'brightness' parameter"));
-            return;
-        }
-        
-        if (doc["brightness"].is<String>()) {
-            String brightnessStr = doc["brightness"];
-            if (brightnessStr.equalsIgnoreCase("auto")) {
-                LED_AUTO_BRIGHTNESS = true;
-                
-                DynamicJsonDocument responseData(128);
-                responseData["brightness"] = "auto";
-                responseData["auto_brightness"] = LED_AUTO_BRIGHTNESS;
-                String dataStr;
-                serializeJson(responseData, dataStr);
-                
-                server.send(200, "application/json", createJsonResponse(true, "Auto brightness enabled", dataStr));
-                return;
-            } else {
-                server.send(400, "application/json", createJsonResponse(false, "Invalid brightness value"));
-                return;
-            }
-        }
-        
-        int brightness = doc["brightness"];
-        if (brightness < 0 || brightness > 255) {
-            server.send(400, "application/json", createJsonResponse(false, "Brightness must be between 0 and 255"));
-            return;
-        }
-        
-        LED_AUTO_BRIGHTNESS = false;  
-        LED_BRIGHTNESS = brightness;
-        if (LED_ENABLED) {
-            leds.setBrightness(LED_BRIGHTNESS);
-            leds.show();
-        }
-        
-        DynamicJsonDocument responseData(128);
-        responseData["brightness"] = LED_BRIGHTNESS;
-        responseData["auto_brightness"] = LED_AUTO_BRIGHTNESS;
-        String dataStr;
-        serializeJson(responseData, dataStr);
-        
-        server.send(200, "application/json", createJsonResponse(true, "Brightness updated", dataStr));
-    }
-
-    void handleSetColor() {
-        #if DEBUG_ENABLED
-            Serial.println(DEBUG_PREFIX_WEB "API call: /api/color");
-        #endif
-        
-        if (!server.hasArg("plain")) {
-            server.send(400, "application/json", createJsonResponse(false, "No JSON body provided"));
-            return;
-        }
-        
-        String body = server.arg("plain");
+    void handleGetState() {
         DynamicJsonDocument doc(512);
-        DeserializationError error = deserializeJson(doc, body);
         
-        if (error) {
-            server.send(400, "application/json", createJsonResponse(false, "Invalid JSON"));
-            return;
-        }
-        
-        if (doc.containsKey("hex")) {
-            String hexColor = doc["hex"];
-            if (hexColor.length() == 7 && hexColor.charAt(0) == '#') {
-                LED_R = strtol(hexColor.substring(1, 3).c_str(), NULL, 16);
-                LED_G = strtol(hexColor.substring(3, 5).c_str(), NULL, 16);
-                LED_B = strtol(hexColor.substring(5, 7).c_str(), NULL, 16);
-            } else {
-                server.send(400, "application/json", createJsonResponse(false, "Invalid hex"));
-                return;
-            }
-        } else if (doc.containsKey("r") && doc.containsKey("g") && doc.containsKey("b")) {
-            LED_R = doc["r"];
-            LED_G = doc["g"];
-            LED_B = doc["b"];
-        } else {
-            server.send(400, "application/json", createJsonResponse(false, "Missing color params"));
-            return;
-        }
-        
-        if (LED_ENABLED) {
-            extern int lastHour, lastMinute;
-            lastHour = -1;
-            lastMinute = -1;
-        }
-        
-        DynamicJsonDocument responseData(256);
-        responseData["color"]["r"] = LED_R;
-        responseData["color"]["g"] = LED_G;
-        responseData["color"]["b"] = LED_B;
+        doc["enabled"] = LED_ENABLED;
+        doc["brightness"] = LED_BRIGHTNESS;
+        doc["auto_brightness"] = LED_AUTO_BRIGHTNESS;
+        doc["color"]["r"] = LED_R;
+        doc["color"]["g"] = LED_G;
+        doc["color"]["b"] = LED_B;
         char hexColor[8];
         sprintf(hexColor, "#%02X%02X%02X", LED_R, LED_G, LED_B);
-        responseData["color"]["hex"] = hexColor;
-        String dataStr;
-        serializeJson(responseData, dataStr);
+        doc["color"]["hex"] = String(hexColor);
         
-        server.send(200, "application/json", createJsonResponse(true, "Color updated", dataStr));
+        String response;
+        serializeJson(doc, response);
+        server.send(200, "application/json", createJsonResponse(true, "State retrieved", response));
+    }
+
+    void handlePostState() {
+        if (!server.hasArg("plain")) {
+            server.send(400, "application/json", createJsonResponse(false, "No JSON body provided"));
+            return;
+        }
+        
+        DynamicJsonDocument doc(512);
+        DeserializationError error = deserializeJson(doc, server.arg("plain"));
+        
+        if (error) {
+            server.send(400, "application/json", createJsonResponse(false, "Invalid JSON"));
+            return;
+        }
+        
+        bool changed = false;
+
+        if (doc.containsKey("enabled")) {
+            LED_ENABLED = doc["enabled"];
+            if (!LED_ENABLED) {
+                leds.clear();
+                leds.show();
+            }
+            changed = true;
+        }
+        
+        if (doc.containsKey("brightness")) {
+            if (doc["brightness"].is<String>() && String((const char*)doc["brightness"]).equalsIgnoreCase("auto")) {
+                LED_AUTO_BRIGHTNESS = true;
+            } else {
+                LED_AUTO_BRIGHTNESS = false;
+                LED_BRIGHTNESS = doc["brightness"];
+            }
+            changed = true;
+        }
+        
+        if (doc.containsKey("color")) {
+            JsonObject color = doc["color"];
+            if (color.containsKey("hex")) {
+                String hexColor = color["hex"];
+                if (hexColor.length() == 7 && hexColor.charAt(0) == '#') {
+                    LED_R = strtol(hexColor.substring(1, 3).c_str(), NULL, 16);
+                    LED_G = strtol(hexColor.substring(3, 5).c_str(), NULL, 16);
+                    LED_B = strtol(hexColor.substring(5, 7).c_str(), NULL, 16);
+                    changed = true;
+                }
+            } else if (color.containsKey("r") && color.containsKey("g") && color.containsKey("b")) {
+                LED_R = color["r"];
+                LED_G = color["g"];
+                LED_B = color["b"];
+                changed = true;
+            }
+        }
+        
+        if (changed) {
+            extern int lastHour, lastMinute;
+            lastHour = -1;
+            lastMinute = -1;
+            if (LED_ENABLED && !LED_AUTO_BRIGHTNESS) {
+                leds.setBrightness(LED_BRIGHTNESS);
+            }
+            saveConfig();
+        }
+        
+        handleGetState();
+    }
+
+    void handleGetConfig() {
+        DynamicJsonDocument doc(1024);
+        
+        doc["led_pin"] = LED_PIN;
+        doc["led_count"] = LED_COUNT;
+        
+        doc["timezone"] = TIMEZONE;
+        doc["ntp_server"] = NTP_SERVER;
+        
+        doc["sensor_sda"] = LIGHT_SENSOR_SDA;
+        doc["sensor_scl"] = LIGHT_SENSOR_SCL;
+        doc["sensor_max_lux"] = LIGHT_SENSOR_MAX_LUX;
+        doc["sensor_min_lux"] = LIGHT_SENSOR_MIN_LUX;
+        doc["sensor_cal_max"] = LIGHT_SENSOR_CALIBRATION_MAX;
+        doc["sensor_min_brightness"] = LIGHT_SENSOR_MIN_BRIGHTNESS;
+        doc["sensor_max_brightness"] = LIGHT_SENSOR_MAX_BRIGHTNESS;
+        
+        doc["update_interval"] = UPDATE_INTERVAL;
+        doc["auto_brightness_enabled"] = AUTO_BRIGHTNESS_ENABLED;
+        doc["web_server_port"] = WEB_SERVER_PORT;
+        
+        String response;
+        serializeJson(doc, response);
+        server.send(200, "application/json", createJsonResponse(true, "Config retrieved", response));
+    }
+
+    void handlePostConfig() {
+        if (!server.hasArg("plain")) {
+            server.send(400, "application/json", createJsonResponse(false, "No JSON body provided"));
+            return;
+        }
+        
+        DynamicJsonDocument doc(1024);
+        DeserializationError error = deserializeJson(doc, server.arg("plain"));
+        
+        if (error) {
+            server.send(400, "application/json", createJsonResponse(false, "Invalid JSON"));
+            return;
+        }
+        
+        if (doc.containsKey("led_pin")) LED_PIN = doc["led_pin"];
+        if (doc.containsKey("led_count")) LED_COUNT = doc["led_count"];
+        
+        if (doc.containsKey("timezone")) TIMEZONE = String((const char*)doc["timezone"]);
+        if (doc.containsKey("ntp_server")) NTP_SERVER = String((const char*)doc["ntp_server"]);
+        
+        if (doc.containsKey("sensor_sda")) LIGHT_SENSOR_SDA = doc["sensor_sda"];
+        if (doc.containsKey("sensor_scl")) LIGHT_SENSOR_SCL = doc["sensor_scl"];
+        if (doc.containsKey("sensor_max_lux")) LIGHT_SENSOR_MAX_LUX = doc["sensor_max_lux"];
+        if (doc.containsKey("sensor_min_lux")) LIGHT_SENSOR_MIN_LUX = doc["sensor_min_lux"];
+        if (doc.containsKey("sensor_cal_max")) LIGHT_SENSOR_CALIBRATION_MAX = doc["sensor_cal_max"];
+        if (doc.containsKey("sensor_min_brightness")) LIGHT_SENSOR_MIN_BRIGHTNESS = doc["sensor_min_brightness"];
+        if (doc.containsKey("sensor_max_brightness")) LIGHT_SENSOR_MAX_BRIGHTNESS = doc["sensor_max_brightness"];
+        
+        if (doc.containsKey("update_interval")) UPDATE_INTERVAL = doc["update_interval"];
+        if (doc.containsKey("auto_brightness_enabled")) AUTO_BRIGHTNESS_ENABLED = doc["auto_brightness_enabled"];
+        if (doc.containsKey("web_server_port")) WEB_SERVER_PORT = doc["web_server_port"];
+        
+        saveConfig();
+        
+        server.send(200, "application/json", createJsonResponse(true, "Config updated. Please restart device for some changes to take effect."));
+    }
+
+    void handleSystemReboot() {
+        server.send(200, "application/json", createJsonResponse(true, "Rebooting..."));
+        delay(500);
+        ESP.restart();
     }
 
     void handleCors() {
@@ -260,32 +226,42 @@ public:
     void begin() {
         server.onNotFound([this]() {
             server.sendHeader("Access-Control-Allow-Origin", "*");
-            server.send(404, "application/json", createJsonResponse(false, "Endpoint not found"));
+            if (server.method() == HTTP_OPTIONS) {
+                this->handleCors();
+            } else {
+                server.send(404, "application/json", createJsonResponse(false, "Endpoint not found"));
+            }
         });
         
         server.on("/api/status", HTTP_GET, [this]() {
             server.sendHeader("Access-Control-Allow-Origin", "*");
-            this->handleStatus();
+            this->handleGetStatus();
         });
         
-        server.on("/api/power", HTTP_POST, [this]() {
+        server.on("/api/state", HTTP_GET, [this]() {
             server.sendHeader("Access-Control-Allow-Origin", "*");
-            this->handlePower();
+            this->handleGetState();
         });
         
-        server.on("/api/brightness", HTTP_POST, [this]() {
+        server.on("/api/state", HTTP_POST, [this]() {
             server.sendHeader("Access-Control-Allow-Origin", "*");
-            this->handleSetBrightness();
+            this->handlePostState();
         });
-        
-        server.on("/api/color", HTTP_POST, [this]() {
+
+        server.on("/api/config", HTTP_GET, [this]() {
             server.sendHeader("Access-Control-Allow-Origin", "*");
-            this->handleSetColor();
+            this->handleGetConfig();
         });
         
-        server.on("/api/power", HTTP_OPTIONS, [this]() { this->handleCors(); });
-        server.on("/api/brightness", HTTP_OPTIONS, [this]() { this->handleCors(); });
-        server.on("/api/color", HTTP_OPTIONS, [this]() { this->handleCors(); });
+        server.on("/api/config", HTTP_POST, [this]() {
+            server.sendHeader("Access-Control-Allow-Origin", "*");
+            this->handlePostConfig();
+        });
+
+        server.on("/api/system/reboot", HTTP_POST, [this]() {
+            server.sendHeader("Access-Control-Allow-Origin", "*");
+            this->handleSystemReboot();
+        });
         
         server.begin();
         #if DEBUG_ENABLED
